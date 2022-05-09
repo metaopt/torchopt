7655d3a7005eb9f9acb4d0e4ddf58b484bcadc7d
Vincent Moens <vincentmoens@gmail.com>
2022-05-09 21:39:12 +0100

    fixes

--- examples/MAML-RL/run_MAML_torchrl.py
+++ examples/MAML-RL/run_MAML_torchrl.py
@@ -30,6 +30,9 @@ inner_iters = 1
 
 
 def a2c_loss(traj, policy, value_coef):
+    dist, *_ = policy.get_dist(traj)
+    traj.set("action_log_prob", dist.log_prob(traj.get("action")))
+
     # Work backwards to compute `G_{T-1}`, ..., `G_0`.
     gae = GAE(GAMMA, LAMBDA, policy, gradient_mode=True)
     traj = gae(traj)
@@ -38,8 +41,6 @@ def a2c_loss(traj, policy, value_coef):
     # advs = lambda_returns - torch.squeeze(values, -1)
     advantage = traj.get("advantage")
     value_target = traj.get("value_target")
-    dist, *_ = policy.get_dist(traj)
-    traj.set("action_log_prob", dist.log_prob(traj.get("action")))
     log_probs = traj.get("action_log_prob")
     action_loss = -(advantage.detach() * log_probs.view_as(advantage)).mean()
     value_loss = value_target.pow(2).mean()
