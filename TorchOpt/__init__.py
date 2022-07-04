b460db45420e583efb110fdddfd84ff77000f3eb
Benjamin-eecs <benjaminliu.eecs@gmail.com>
2022-07-04 15:48:02 +0800

    fix(all): pass lint

--- TorchOpt/__init__.py
+++ TorchOpt/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+"""TorchOpt: a high-performance optimizer library built upon PyTorch."""
 
 from TorchOpt._src import (
   accelerated_op_available,
