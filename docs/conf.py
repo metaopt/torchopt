509eb39db5770bcc9b61031a63ae34e8f782af82
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-07-13 16:22:01 +0800

    docs(image): rename image files

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_logo = "_static/images/logod-05.png"
+html_logo = "_static/images/logo.png"
 
 
 def setup(app):
