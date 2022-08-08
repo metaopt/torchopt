92fa5c727eae5299e6d0c911c0fe2f59997532e7
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-08-09 00:35:34 +0800

    test: reduce number of tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -32,8 +32,18 @@ NUM_UPDATES = 3
 
 
 def parametrize(**argvalues) -> pytest.mark.parametrize:
-    arguments = tuple(argvalues)
-    argvalues = tuple(itertools.product(*tuple(map(argvalues.get, arguments))))
+    arguments = list(argvalues)
+
+    if 'dtype' in argvalues:
+        dtypes = argvalues['dtype']
+        argvalues['dtype'] = dtypes[:1]
+        arguments.remove('dtype')
+        arguments.insert(0, 'dtype')
+
+        argvalues = list(itertools.product(*tuple(map(argvalues.get, arguments))))
+        first_product = argvalues[0]
+        argvalues.extend((dtype,) + first_product[1:] for dtype in dtypes[1:])
+
     ids = tuple(
         '-'.join(f'{arg}({val})' for arg, val in zip(arguments, values)) for values in argvalues
     )
