12c30a281c6808dfe901a0781ddef7d4750d3f1e
Jie Ren <jieren9806@gmail.com>
2022-08-09 21:32:42 +0800

    feat(implicit): allow argnums

--- TorchOpt/_src/implicit_diff.py
+++ TorchOpt/_src/implicit_diff.py
@@ -17,22 +17,22 @@ import inspect
 from typing import Any
 from typing import Callable
 from typing import Optional
+from typing import Union
 from typing import Tuple
 
+import functorch
 import jax
 import torch
-from TorchOpt._src import linear_solve
 from torch.autograd import Function
-from torch.autograd.functional import jvp, vjp
-import functorch
-from functools import partial
+
+from TorchOpt._src import linear_solve
 
 
 def root_vjp(optimality_fun: Callable,
              sol: Any,
              args: Tuple,
              cotangent: Any,
-             mask: Tuple,
+             argnums: Tuple,
              solve: Callable = linear_solve.solve_normal_cg) -> Any:
     """Vector-Jacobian product of a root.
 
@@ -68,23 +68,24 @@ def root_vjp(optimality_fun: Callable,
     u = solve(matvec, v)
 
     class MaskArgs:
-        def __init__(self, mask, *args) -> None:
-            self.mask = mask
+        def __init__(self, argnums, *args) -> None:
+            self.argnums = argnums
             pre_filled = []
             post_filled = []
-            for is_tensor, arg in zip(mask, args):
-                if is_tensor:
+            for idx, arg in enumerate(args):
+                if idx + 1 in argnums:
                     post_filled.append(arg)
                 else:
                     pre_filled.append(arg)
             self.pre_filled = pre_filled
             self.post_filled = post_filled
+            self.len_args = len(self.pre_filled) + len(self.post_filled)
 
         def __call__(self, *args) -> Any:
             args = list(args)
             true_args = []
-            for is_tensor in self.mask:
-                if is_tensor:
+            for idx in range(self.len_args):
+                if idx + 1 in self.argnums:
                     arg = args.pop(0)
                 else:
                     arg = self.pre_filled.pop(0)
@@ -95,18 +96,19 @@ def root_vjp(optimality_fun: Callable,
     #     # We close over the solution.
     #     return optimality_fun(sol, *args)
 
-    fun_args = MaskArgs(mask[1:], *args)
+    fun_args = MaskArgs(argnums, *args)
 
     _, vjp_fun_args = functorch.vjp(fun_args, *fun_args.post_filled)
 
     result = vjp_fun_args(u)
     result = list(result)
     true_result = [None]
-    for is_tensor in mask[1:]:
-        if not is_tensor:
-            true_result.append(None)
-        else:
+    for idx in range(fun_args.len_args):
+        if idx + 1 in argnums:
             true_result.append(result.pop(0))
+        else:
+            true_result.append(None)
+
     return tuple(true_result)
 
 
@@ -157,7 +159,7 @@ def _signature_bind_and_match(signature, *args, **kwargs):
     return out_args, out_kwargs, map_back
 
 
-def _custom_root(solver_fun, optimality_fun, solve, has_aux,
+def _custom_root(solver_fun, optimality_fun, solve, argnums, has_aux,
                  reference_signature=None):
     # When caling through `jax.custom_vjp`, jax attempts to resolve all
     # arguments passed by keyword to positions (this is in order to
@@ -189,30 +191,39 @@ def _custom_root(solver_fun, optimality_fun, solve, has_aux,
         fun = getattr(reference_signature, "subfun", reference_signature)
         reference_signature = inspect.signature(fun)
 
-    def make_custom_vjp_solver_fun(solver_fun, kwarg_keys):
+    def make_custom_vjp_solver_fun(solver_fun, kwarg_keys, args_sign):
         class ImplicitMetaGradient(Function):
             @staticmethod
             def forward(ctx, *flat_args):
-                args, kwargs = _extract_kwargs(kwarg_keys, flat_args)
-                ctx.mask = tuple([isinstance(term, torch.Tensor) for term in flat_args])
+                args = []
+                for idx, (start_point, is_tuple) in enumerate(args_sign):
+                    if is_tuple:
+                        args.append(tuple(flat_args[start_point:args_sign[idx+1][0]]))
+                    else:
+                        args.append(flat_args[start_point])
+                args = tuple(args)
+
+                args, kwargs = _extract_kwargs(kwarg_keys, args)
                 res = solver_fun(*args, **kwargs)
-                ctx.aux = (res, flat_args)
-                return res
+                if has_aux:
+                    aux = res[1]
+                    res = res[0]
+                    ctx.aux = (res, args)
+                    return res, aux
+                else:
+                    ctx.aux = (res, args)
+                    return res
 
             @staticmethod
             def backward(ctx, *cotangent):
                 res, flat_args = ctx.aux
                 ctx.aux = None
-                mask = ctx.mask
-                ctx.mask = None
                 args, kwargs = _extract_kwargs(kwarg_keys, flat_args)
 
                 # solver_fun can return auxiliary data if has_aux = True.
                 if has_aux:
                     cotangent = cotangent[0]
-                    sol = res[0]
-                else:
-                    sol = res
+                sol = res
                 ba_args, ba_kwargs, map_back = _signature_bind_and_match(
                     reference_signature, *args, **kwargs)
                 if ba_kwargs:
@@ -226,23 +237,56 @@ def _custom_root(solver_fun, optimality_fun, solve, has_aux,
 
                 # Compute VJPs w.r.t. args.
                 vjps = root_vjp(optimality_fun=optimality_fun, sol=sol,
-                                args=ba_args[1:], cotangent=cotangent, mask=mask, solve=solve)
+                                args=ba_args[1:], cotangent=cotangent, argnums=argnums, solve=solve)
                 # Prepend None as the vjp for init_params.
 
                 arg_vjps, kws_vjps = map_back(vjps)
                 ordered_vjps = tuple(arg_vjps) + tuple(kws_vjps[k] for k in kwargs.keys())
-                return ordered_vjps
+                true_vjps = []
+                for idx, ((_, is_tuple), vjp) in enumerate(zip(args_sign, ordered_vjps)):
+                    if is_tuple:
+                        for item in vjp:
+                            true_vjps.append(item)
+                    else:
+                        true_vjps.append(vjp)
+                return tuple(true_vjps)
         return ImplicitMetaGradient
 
     def wrapped_solver_fun(*args, **kwargs):
         args, kwargs = _signature_bind(solver_fun_signature, *args, **kwargs)
         keys, vals = list(kwargs.keys()), list(kwargs.values())
-        return make_custom_vjp_solver_fun(solver_fun, keys).apply(*args, *vals)
+
+        args_sign = []
+        flatten_args = []
+        args_counter = 0
+        for idx, arg in enumerate(args):
+            if idx in argnums:
+                if isinstance(arg, torch.Tensor):
+                    args_sign.append((args_counter, False))  # start position, is_tuple
+                    flatten_args.append(arg)
+                    args_counter += 1
+                elif isinstance(arg, tuple):
+                    args_sign.append((args_counter, True))  # start position, is_tuple
+                    for arg_item in arg:
+                        flatten_args.append(arg_item)
+                    args_counter += len(arg)
+                else:
+                    raise RuntimeError("must be tensor or tensor tuple")
+            else:
+                args_sign.append((args_counter, False))  # start position, is_tuple
+                flatten_args.append(arg)
+                args_counter += 1
+
+        args_sign = tuple(args_sign)
+        flatten_args = tuple(flatten_args)
+
+        return make_custom_vjp_solver_fun(solver_fun, keys, args_sign).apply(*flatten_args, *vals)
 
     return wrapped_solver_fun
 
 
 def custom_root(optimality_fun: Callable,
+                argnums: Union[tuple, int] = 0,
                 has_aux: bool = False,
                 solve: Callable = None,
                 reference_signature: Optional[Callable] = None):
@@ -268,11 +312,14 @@ def custom_root(optimality_fun: Callable,
       A solver function decorator, i.e.,
       ``custom_root(optimality_fun)(solver_fun)``.
     """
+    if isinstance(argnums, int):
+        argnums = (argnums,)
+
     if solve is None:
         solve = linear_solve.solve_normal_cg
 
     def wrapper(solver_fun):
-        return _custom_root(solver_fun, optimality_fun, solve, has_aux,
+        return _custom_root(solver_fun, optimality_fun, solve, argnums, has_aux,
                             reference_signature)
 
     return wrapper
