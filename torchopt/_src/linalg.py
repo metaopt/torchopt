5fe9129e8aaacd0a4e704e1cd2688e82df937ff0
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-09-06 17:24:30 +0800

    wip

--- torchopt/_src/linalg.py
+++ torchopt/_src/linalg.py
@@ -34,14 +34,16 @@
 
 import math
 from functools import partial
+from typing import Any, Callable, List, Optional, Sequence, Union
 
 import torch
 
+from torchopt._src.typing import TensorTree
 from torchopt._src.utils import pytree
 
 
 # Aliases for working with pytrees
-def _vdot_real_part(x, y):
+def _vdot_real_part(x: torch.Tensor, y: torch.Tensor) -> torch.FloatTensor:
     x = x.view(-1)
     y = y.view(-1)
     result = torch.dot(x.real, y.real)
@@ -50,15 +52,17 @@ def _vdot_real_part(x, y):
     return result
 
 
-def _vdot_real_tree(x, y):
+def _vdot_real_tree(x: TensorTree, y: TensorTree) -> TensorTree:
     return pytree.tree_map(_vdot_real_part, x, y)
 
 
-def _identity(x):
+def _identity(x: TensorTree) -> TensorTree:
     return x
 
 
-def _normalize_matvec(f):
+def _normalize_matvec(
+    f: Union[Callable[[TensorTree], TensorTree], torch.Tensor]
+) -> Callable[[TensorTree], TensorTree]:
     """Normalize an argument for computing matrix-vector products."""
     if callable(f):
         return f
@@ -69,21 +73,28 @@ def _normalize_matvec(f):
     return partial(torch.matmul, f)
 
 
-def _safe_sum(obj):
-    if isinstance(obj, tuple):
+def _safe_sum(obj: Union[Any, Sequence[Any]]) -> Any:
+    if isinstance(obj, (list, tuple)):
         return sum(obj)
     return obj
 
 
 # pylint: disable-next=too-many-locals
-def _cg_solve(A, b, x0=None, *, maxiter, rtol=1e-5, atol=0.0, M=_identity):
+def _cg_solve(
+    A: Callable[[TensorTree], TensorTree],
+    b: TensorTree,
+    x0: Optional[TensorTree] = None,
+    *,
+    maxiter: int,
+    rtol: float = 1e-5,
+    atol: float = 0.0,
+    M: Callable[[TensorTree], TensorTree] = _identity,
+) -> TensorTree:
+    # https://en.wikipedia.org/wiki/Conjugate_gradient_method#The_preconditioned_conjugate_gradient_method
+
     # tolerance handling uses the "non-legacy" behavior of `scipy.sparse.linalg.cg`
     bs = _safe_sum(_vdot_real_tree(b, b))
     atol2 = max(rtol**2 * bs, atol**2)
-
-    # https://en.wikipedia.org/wiki/Conjugate_gradient_method#The_preconditioned_conjugate_gradient_method
-
-    min_rs = math.inf
 
     def cond_fun(value, min_rs):
         _, r, gamma, _, k = value
@@ -105,9 +116,9 @@ def _cg_solve(A, b, x0=None, *, maxiter, rtol=1e-5, atol=0.0, M=_identity):
     r0 = pytree.tree_map(torch.sub, b, A(x0))
     p0 = z0 = M(r0)
     gamma0 = _safe_sum(_vdot_real_tree(r0, z0))
-    initial_value = (x0, r0, gamma0, p0, 0)
 
-    value = initial_value
+    min_rs = math.inf
+    value = (x0, r0, gamma0, p0, 0)
     not_stop, min_rs = cond_fun(value, min_rs)
     while not_stop:
         value = body_fun(value)
@@ -119,18 +130,28 @@ def _cg_solve(A, b, x0=None, *, maxiter, rtol=1e-5, atol=0.0, M=_identity):
     return x_final
 
 
-def _shapes(tree):
-    flattened_tree = pytree.tree_leaves(tree)
-    return pytree.tree_leaves([tuple(term.shape) for term in flattened_tree])
+def _shapes(tree: TensorTree) -> List[int]:
+    flattened = pytree.tree_leaves(tree)
+    return pytree.tree_leaves([tuple(term.shape) for term in flattened])
 
 
-def _isolve(_isolve_solve, A, b, x0=None, *, rtol=1e-5, atol=0.0, maxiter=None, M=None):
+def _isolve(
+    _isolve_solve: Callable,
+    A: Union[TensorTree, Callable[[TensorTree], TensorTree]],
+    b: TensorTree,
+    x0: Optional[TensorTree] = None,
+    *,
+    rtol: float = 1e-5,
+    atol: float = 0.0,
+    maxiter: Optional[int] = None,
+    M: Optional[Union[TensorTree, Callable[[TensorTree], TensorTree]]] = None,
+):
     if x0 is None:
         x0 = pytree.tree_map(torch.zeros_like, b)
 
     if maxiter is None:
         size = sum(_shapes(b))
-        maxiter = size  # copied from scipy
+        maxiter = size  # copied from SciPy
 
     if M is None:
         M = _identity
@@ -145,11 +166,19 @@ def _isolve(_isolve_solve, A, b, x0=None, *, rtol=1e-5, atol=0.0, maxiter=None, 
     isolve_solve = partial(_isolve_solve, x0=x0, tol=rtol, atol=atol, maxiter=maxiter, M=M)
 
     x = isolve_solve(A, b)
-    info = None
-    return x, info
+    return x
 
 
-def cg(A, b, x0=None, *, rtol=1e-5, atol=0.0, maxiter=None, M=None):
+def cg(
+    A: Union[TensorTree, Callable[[TensorTree], TensorTree]],
+    b: TensorTree,
+    x0: Optional[TensorTree] = None,
+    *,
+    rtol: float = 1e-5,
+    atol: float = 0.0,
+    maxiter: Optional[int] = None,
+    M: Optional[Union[TensorTree, Callable[[TensorTree], TensorTree]]] = None,
+):
     """Use Conjugate Gradient iteration to solve ``Ax = b``.
 
     The numerics of JAX's ``cg`` should exact match SciPy's ``cg`` (up to numerical precision), but
@@ -161,20 +190,15 @@ def cg(A, b, x0=None, *, rtol=1e-5, atol=0.0, maxiter=None, M=None):
     solves converge.
 
     Args:
-        A: (ndarray or function)
+        A: (tensor or tree of tensors or function)
             2D array or function that calculates the linear map (matrix-vector
             product) ``Ax`` when called like ``A(x)``. ``A`` must represent a
             hermitian, positive definite matrix, and must return array(s) with the
             same structure and shape as its argument.
-        b: (array or tree of arrays)
+        b: (tensor or tree of tensors)
             Right hand side of the linear system representing a single vector. Can be
             stored as an array or Python container of array(s) with any shape.
-            x: array or tree of arrays
-            The converged solution. Has the same structure as ``b``.
-        info: None
-            Placeholder for convergence information. In the future, JAX will report the number of
-            iterations when convergence is not achieved, like SciPy.
-        x0: (array)
+        x0: (tensor or tree of tensors, optional)
             Starting guess for the solution. Must have the same structure as ``b``.
         rtol: (float, optional, default: :const:`1e-5`)
             Tolerances for convergence, ``norm(residual) <= max(rtol*norm(b), atol)``. We do not
@@ -184,15 +208,15 @@ def cg(A, b, x0=None, *, rtol=1e-5, atol=0.0, maxiter=None, M=None):
             Tolerances for convergence, ``norm(residual) <= max(tol*norm(b), atol)``. We do not
             implement SciPy's "legacy" behavior, so JAX's tolerance will differ from SciPy unless
             you explicitly pass ``atol`` to SciPy's ``cg``.
-        maxiter: (integer)
+        maxiter: (integer, optional)
             Maximum number of iterations. Iteration will stop after maxiter steps even if the
             specified tolerance has not been achieved.
-        M: (ndarray or function)
+        M: (tensor or tree of tensors or function)
             Pre-conditioner for ``A``. The pre-conditioner should approximate the inverse of ``A``.
             Effective preconditioning dramatically improves the rate of convergence, which implies
             that fewer iterations are needed to reach a given error tolerance.
 
     Returns:
-        the CG linear solver
+        the Conjugate Gradient (CG) linear solver
     """
     return _isolve(_cg_solve, A=A, b=b, x0=x0, rtol=rtol, atol=atol, maxiter=maxiter, M=M)
