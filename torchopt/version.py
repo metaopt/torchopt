dff6188a6723ff91cd9e469fb71185a7e87f9704
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-09-09 16:04:24 +0800

    fix: change counts to scalar tensors (#71)

--- torchopt/version.py
+++ torchopt/version.py
@@ -15,3 +15,37 @@
 """TorchOpt: a high-performance optimizer library built upon PyTorch."""
 
 __version__ = '0.5.0'
+__license__ = 'Apache License, Version 2.0'
+__author__ = 'TorchOpt Contributors'
+__release__ = False
+
+if not __release__:
+    import os
+    import subprocess
+
+    try:
+        prefix, sep, suffix = (
+            subprocess.check_output(
+                ['git', 'describe', '--abbrev=7'],
+                cwd=os.path.dirname(os.path.abspath(__file__)),
+                stderr=subprocess.DEVNULL,
+                universal_newlines=True,
+            )
+            .strip()
+            .lstrip('v')
+            .replace('-', '.dev', 1)
+            .replace('-', '+', 1)
+            .partition('.dev')
+        )
+        if sep:
+            version_prefix, dot, version_tail = prefix.rpartition('.')
+            prefix = f'{version_prefix}{dot}{int(version_tail) + 1}'
+            __version__ = sep.join((prefix, suffix))
+            del version_prefix, dot, version_tail
+        else:
+            __version__ = prefix
+        del prefix, sep, suffix
+    except (OSError, subprocess.CalledProcessError):
+        pass
+
+    del os, subprocess
