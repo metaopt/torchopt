583ddef8f201d7971d5fda20d5c2f148a97dc9d7
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-10-26 18:00:37 +0800

    feat: enable parallel build

---
+++ .github/workflows/set_cibw_build.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+# pylint: disable-next=consider-using-f-string
+CIBW_BUILD = 'CIBW_BUILD=*cp%d%d-*manylinux*' % sys.version_info[:2]
+
+print(CIBW_BUILD)
+with open(os.getenv('GITHUB_ENV'), mode='at', encoding='UTF-8') as file:
+    print(CIBW_BUILD, file=file)
