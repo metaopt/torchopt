a6cfa4c1c281787087253bf2fd9dc43a83f19b7a
Benjamin-eecs <benjaminliu.eecs@gmail.com>
2023-01-19 02:04:29 +0800

    fix: pass lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -144,8 +144,8 @@ simplify_optional_unions = False
 
 # -- Options for autosummary ---------------------------------------------------
 
-autosummary_generate = False
-numpydoc_class_members_toctree = False
+autosummary_generate = True
+# numpydoc_class_members_toctree = False
 
 # -- Options for bibtex --------------------------------------------------------
 
