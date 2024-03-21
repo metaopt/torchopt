ae04ce0464d3c1ff353754cd59029aae9d6e638d
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-03-21 13:48:41 +0000

    chore: update license header

--- examples/LOLA/visualize.py
+++ examples/LOLA/visualize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 MetaOPT Team. All Rights Reserved.
+# Copyright 2022-2024 MetaOPT Team. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
